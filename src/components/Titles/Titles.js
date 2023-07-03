6b946658a6c323ca904a9d1d06349699b514f530
marditds <marditds@gmail.com>
2023-07-02 21:33:31 -0700

    styling

--- src/components/Titles/Titles.js
+++ src/components/Titles/Titles.js
@@ -1,21 +1,51 @@
 import React, { useState, useEffect } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import { titlesData } from './titlesData';
+import { LuChevronLeft, LuChevronRight } from 'react-icons/lu';
+import './Titles.css';
 
 export const Titles = () => {
 
     const [titles, setTitles] = useState(titlesData);
-    const [currentIndex, setCurrentIndex] = useState(0);
+    const [currIdx, setCurrIdx] = useState(0);
+
+    const handlePrevTitle = () => {
+        setCurrIdx((preVal) => preVal - 1);
+        if (currIdx < 1) {
+            setCurrIdx((preVal) => titles.length - 1)
+        }
+    }
+
+    const handleNextTitle = () => {
+        setCurrIdx((preVal) => preVal + 1);
+        if (currIdx > 6) {
+            setCurrIdx((preVal) => 0)
+        }
+    }
+
+    console.log("THIS IS INDEX: " + currIdx + " " + titles[currIdx]);
 
 
     return (
         <Row>
-            <Col>I'm a {
-                titles.map((title, idx) => {
-                    return (
-                        <span key={idx}>{title} </span>)
-                })
-            }
+            <Col>I'm a
+                <LuChevronLeft
+                    size={25}
+                    onClick={handlePrevTitle}
+                    className='titles--btn'
+                />
+                {
+                    titles.slice(currIdx, currIdx + 1).map((title, idx) => {
+                        return (
+                            <span key={idx}> {title} </span>
+                        )
+                    })
+                }
+                <LuChevronRight
+                    size={25}
+                    onClick={handleNextTitle}
+                    className='titles--btn'
+                />
             </Col>
         </Row>
     )
