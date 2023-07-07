88977a348d9693b1122f422a93509e9fba1eb634
marditds <marditds@gmail.com>
2023-07-07 15:13:18 -0700

    tools styling

--- src/components/Resume/Skills/ToolsAndSoftware/ToolsAndSoftware.js
+++ src/components/Resume/Skills/ToolsAndSoftware/ToolsAndSoftware.js
@@ -8,35 +8,53 @@ export const ToolsAndSoftware = () => {
         <Row className='justify-content-center mt-3 mb-4'>
             <Row xs={2} sm={3} className='icon--row'>
                 <Col className='icon--col'>
-                    <SiGithub size={55} />GitHub
+                    <SiGithub size={55} />
+                    <SiGithub size={55} />
+                    GitHub
                 </Col>
                 <Col className='icon--col'>
-                    <SiJira size={55} />Jira
+                    <SiJira size={55} />
+                    <SiJira size={55} />
+                    Jira
                 </Col>
                 <Col className='icon--col my-4 my-sm-0'>
-                    < SiFigma size={55} />Figma
+                    < SiFigma size={55} />
+                    < SiFigma size={55} />
+                    Figma
                 </Col>
                 <Col className='icon--col my-4 my-sm-0 d-grid d-sm-none'>
-                    <SiGitlab size={55} />GitLab
+                    <SiGitlab size={55} />
+                    <SiGitlab size={55} />
+                    GitLab
                 </Col>
             </Row>
             <Row xs={2} sm={3} className='my-sm-4 icon--row'>
                 <Col className='icon--col d-none d-sm-grid'>
-                    <SiGitlab size={55} />GitLab
+                    <SiGitlab size={55} />
+                    <SiGitlab size={55} />
+                    GitLab
                 </Col>
                 <Col className='icon--col'>
-                    <SiVisualstudiocode size={55} />Visual Studio Code
+                    <SiVisualstudiocode size={55} />
+                    <SiVisualstudiocode size={55} />
+                    Visual Studio Code
                 </Col>
                 <Col className='icon--col' >
-                    <SiAdobedreamweaver size={55} />Adobe Dreamweaver
+                    <SiAdobedreamweaver size={55} />
+                    <SiAdobedreamweaver size={55} />
+                    Adobe Dreamweaver
                 </Col>
             </Row>
             <Row xs={2} sm={3} className='icon--row'>
                 <Col className='icon--col my-4 my-sm-0'>
-                    <SiAdobephotoshop size={55} />Adobe Photoshop
+                    <SiAdobephotoshop size={55} />
+                    <SiAdobephotoshop size={55} />
+                    Adobe Photoshop
                 </Col>
                 <Col className='icon--col my-4 my-sm-0'>
-                    <SiEclipseide size={55} />Eclipse
+                    <SiEclipseide size={55} />
+                    <SiEclipseide size={55} />
+                    Eclipse
                 </Col>
             </Row>
         </Row>
