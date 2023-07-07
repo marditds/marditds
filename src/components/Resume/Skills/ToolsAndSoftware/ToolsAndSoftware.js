8c2cd226c28d60ca8043fc8d14607efa564fb984
marditds <marditds@gmail.com>
2023-07-07 16:20:29 -0700

    icons

--- src/components/Resume/Skills/ToolsAndSoftware/ToolsAndSoftware.js
+++ src/components/Resume/Skills/ToolsAndSoftware/ToolsAndSoftware.js
@@ -9,21 +9,17 @@ export const ToolsAndSoftware = () => {
             <Row xs={2} sm={3} className='icon--row'>
                 <Col className='icon--col'>
                     <SiGithub size={55} />
-                    <SiGithub size={55} />
                     GitHub
                 </Col>
                 <Col className='icon--col'>
-                    <SiJira size={55} />
                     <SiJira size={55} />
                     Jira
                 </Col>
                 <Col className='icon--col my-4 my-sm-0'>
                     < SiFigma size={55} />
-                    < SiFigma size={55} />
                     Figma
                 </Col>
                 <Col className='icon--col my-4 my-sm-0 d-grid d-sm-none'>
-                    <SiGitlab size={55} />
                     <SiGitlab size={55} />
                     GitLab
                 </Col>
@@ -31,16 +27,13 @@ export const ToolsAndSoftware = () => {
             <Row xs={2} sm={3} className='my-sm-4 icon--row'>
                 <Col className='icon--col d-none d-sm-grid'>
                     <SiGitlab size={55} />
-                    <SiGitlab size={55} />
                     GitLab
                 </Col>
                 <Col className='icon--col'>
                     <SiVisualstudiocode size={55} />
-                    <SiVisualstudiocode size={55} />
                     Visual Studio Code
                 </Col>
                 <Col className='icon--col' >
-                    <SiAdobedreamweaver size={55} />
                     <SiAdobedreamweaver size={55} />
                     Adobe Dreamweaver
                 </Col>
@@ -48,11 +41,9 @@ export const ToolsAndSoftware = () => {
             <Row xs={2} sm={3} className='icon--row'>
                 <Col className='icon--col my-4 my-sm-0'>
                     <SiAdobephotoshop size={55} />
-                    <SiAdobephotoshop size={55} />
                     Adobe Photoshop
                 </Col>
                 <Col className='icon--col my-4 my-sm-0'>
-                    <SiEclipseide size={55} />
                     <SiEclipseide size={55} />
                     Eclipse
                 </Col>
