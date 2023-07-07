a26d7267f0f7c606f6e0bef3ce58d07940ca3a1e
marditds <marditds@gmail.com>
2023-07-07 14:23:27 -0700

    Update Programming.js

--- src/components/Resume/Skills/Programming/Programming.js
+++ src/components/Resume/Skills/Programming/Programming.js
@@ -10,26 +10,58 @@ export const Programming = () => {
 
             <Row xs={2} sm={3}>
                 <Col className='icon--col my-4 my-sm-0'>
-                    <SiReact className='react--icon' size={55} />
-                    <SiReact className='react--icon' size={55} />
+                    <SiReact size={55} />
+                    <SiReact size={55} />
                     ReactJS
                 </Col>
                 <Col className='icon--col my-4 my-sm-0'>
                     <SiBootstrap size={55} />
                     <SiBootstrap size={55} />
                     Bootstrap</Col>
-                <Col className='icon--col'><SiHtml5 size={55} />HTML5</Col>
-                <Col className='icon--col d-grid d-sm-none'><SiCss3 size={55} />CSS3</Col>
+                <Col className='icon--col'>
+                    <SiHtml5 size={55} />
+                    <SiHtml5 size={55} />
+                    HTML5
+                </Col>
+                <Col className='icon--col d-grid d-sm-none'>
+                    <SiCss3 size={55} />
+                    <SiCss3 size={55} />
+                    CSS3
+                </Col>
             </Row>
             <Row xs={2} sm={3} className='my-sm-4'>
-                <Col className='icon--col d-none d-sm-grid'><SiCss3 size={55} />CSS3</Col>
-                <Col className='icon--col my-4 my-sm-0'><SiJavascript size={55} />JavaScript</Col>
-                <Col className='icon--col my-4 my-sm-0'>< SiNodedotjs size={55} />NodeJS</Col>
+                <Col className='icon--col d-none d-sm-grid'>
+                    <SiCss3 size={55} />
+                    <SiCss3 size={55} />
+                    CSS3
+                </Col>
+                <Col className='icon--col my-4 my-sm-0'>
+                    <SiJavascript size={55} />
+                    <SiJavascript size={55} />
+                    JavaScript
+                </Col>
+                <Col className='icon--col my-4 my-sm-0'>
+                    < SiNodedotjs size={55} />
+                    < SiNodedotjs size={55} />
+                    NodeJS
+                </Col>
             </Row>
             <Row xs={2} sm={3}>
-                <Col className='icon--col'><SiPhp size={55} />PHP</Col>
-                <Col className='icon--col'><SiMysql size={55} />MySQL</Col>
-                <Col className='icon--col my-4 my-sm-0'><BiLogoJava size={55} />Java</Col>
+                <Col className='icon--col'>
+                    <SiPhp size={55} />
+                    <SiPhp size={55} />
+                    PHP
+                </Col>
+                <Col className='icon--col'>
+                    <SiMysql size={55} />
+                    <SiMysql size={55} />
+                    MySQL
+                </Col>
+                <Col className='icon--col my-4 my-sm-0'>
+                    <BiLogoJava size={55} />
+                    <BiLogoJava size={55} />
+                    Java
+                </Col>
             </Row>
         </Row>
 
