cf2936ed525d80270c45c1a7feb88bf847af23bb
marditds <marditds@gmail.com>
2023-07-07 23:14:04 -0700

    Update Skills.js

--- src/components/Resume/Skills/Skills.js
+++ src/components/Resume/Skills/Skills.js
@@ -7,9 +7,9 @@ import { ToolsAndSoftware } from './ToolsAndSoftware/ToolsAndSoftware';
 export const Skills = () => {
     return (
         <>
-            <Container fluid className='prgrm--row'>
+            <Container fluid className='prgrm--row pt-3 pb-3'>
                 <Container >
-                    <section className='pt-3 pb-3'>
+                    <section className=''>
                         <h3 className='mb-0 pb-0'>Programming Languages</h3>
                         <Programming />
                     </section>
