35633df7c26fcf0572e8d535109a43e217e59a1d
marditds <marditds@gmail.com>
2023-07-08 00:39:44 -0700

    styling

--- src/components/Resume/Education/Education.js
+++ src/components/Resume/Education/Education.js
@@ -1,52 +1,54 @@
 import React from 'react';
-import { Row, Col, Image } from 'react-bootstrap';
+import { Container, Row, Col, Image } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './Education.css';
-import CSUNLogo from '../../../assets/csunCSLogo.jpg';
+import CSUNLogo from '../../../assets/csunCSLogo.png';
 import { RiComputerLine } from 'react-icons/ri';
 import { PiComputerTowerBold } from 'react-icons/pi';
 import { PiComputerTower } from 'react-icons/pi';
 
 export const Education = () => {
     return (
-
-        <section className='education--content'>
-            <h3>Education</h3>
-            <Row>
-                <Col>
-                    <Col xs={12} sm={8}>
-                        <h5 className='fw-bold mb-0'>
-                            Bachelor of Science in Computer Science
-                            <RiComputerLine
-                                className='education--icon'
-                                size={35} />
-                            <PiComputerTowerBold
-                                className='education--icon'
-                                size={35} />
-                        </h5>
-                    </Col>
-                    <Col>
-                        <Link to={`https://www.csun.edu`} target='_blank'>
-                            <Image src={CSUNLogo} className='school--logo' fluid />
-                        </Link>
-                    </Col>
-                </Col>
-                <Col xs={12} sm={4} className='d-flex justify-content-end'>August 2022</Col>
-            </Row>
-            <Row className='mt-4'>
-                <Col>
-                    <Col xs={12} sm={8}>
-                        <span className='fw-bold'>Associate of Science in Web Development</span>
-                    </Col>
-                    <Col>
-                        <Link to={`https://www.glendale.edu`} target='_blank'>
-                            Glendale Community College
-                        </Link>
-                    </Col>
-                </Col>
-                <Col xs={12} sm={4} className='d-flex justify-content-end'>August 2020</Col>
-            </Row>
-        </section>
-
+        <Container fluid className='education--row pt-3 pb-3'>
+            <Container>
+                <section className='education--content'>
+                    <h3>Education</h3>
+                    <Row>
+                        <Col>
+                            <Col xs={12} sm={8}>
+                                <h5 className='fw-bold mb-0'>
+                                    Bachelor of Science in Computer Science
+                                    <RiComputerLine
+                                        className='education--icon'
+                                        size={35} />
+                                    <PiComputerTowerBold
+                                        className='education--icon'
+                                        size={35} />
+                                </h5>
+                            </Col>
+                            <Col>
+                                <Link to={`https://www.csun.edu`} target='_blank'>
+                                    <Image src={CSUNLogo} className='school--logo' fluid />
+                                </Link>
+                            </Col>
+                        </Col>
+                        <Col xs={12} sm={4} className='d-flex justify-content-end'>August 2022</Col>
+                    </Row>
+                    <Row className='mt-4'>
+                        <Col>
+                            <Col xs={12} sm={8}>
+                                <span className='fw-bold'>Associate of Science in Web Development</span>
+                            </Col>
+                            <Col>
+                                <Link to={`https://www.glendale.edu`} target='_blank'>
+                                    Glendale Community College
+                                </Link>
+                            </Col>
+                        </Col>
+                        <Col xs={12} sm={4} className='d-flex justify-content-end'>August 2020</Col>
+                    </Row>
+                </section>
+            </Container>
+        </Container>
     )
 }
--- src/components/Resume/Skills/Skills.js
+++ src/components/Resume/Skills/Skills.js
@@ -8,17 +8,17 @@ export const Skills = () => {
     return (
         <>
             <Container fluid className='prgrm--row pt-3 pb-3'>
-                <Container >
-                    <section className=''>
+                <Container>
+                    <section>
                         <h3 className='mb-0 pb-0'>Programming Languages</h3>
                         <Programming />
                     </section>
                 </Container>
             </Container>
 
-            <Container fluid className='tools--row'>
-                <Container >
-                    <section className='pt-3 pb-3'>
+            <Container fluid className='tools--row pt-3 pb-3'>
+                <Container>
+                    <section>
                         <h3 className='mb-0 pb-0'>Tools & Software</h3>
                         <ToolsAndSoftware />
                     </section>
