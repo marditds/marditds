638b05364523d149f63932d18c166897c2678614
marditds <marditds@gmail.com>
2023-07-09 15:11:04 -0700

    icon fix

--- src/components/Resume/Education/Education.js
+++ src/components/Resume/Education/Education.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import './Education.css';
 import CSUNLogo from '../../../assets/csunCSLogo.png';
 import { RiComputerLine } from 'react-icons/ri';
-import { PiComputerTowerBold } from 'react-icons/pi';
+import { PiDesktop, PiComputerTower, PiCertificate, PiCertificateBold } from 'react-icons/pi';
 
 export const Education = () => {
 
@@ -30,49 +30,23 @@ export const Education = () => {
                 <Container>
                     <section className=''>
                         <h3>Education</h3>
-                        <Row>
-                            <Col>
-                                <Col xs={12} sm={8}>
-                                    <p className='fw-bold d-flex align-items-end mb-0'>
-                                        <span className='d-none d-sm-inline'>
-                                            Bachelor of Science{" "}
-                                        </span>
-                                        <span className='d-inline d-sm-none'>
-                                            BS{" "}
-                                        </span>
-                                        in Computer Science
 
-                                        <span className='d-inline-block'>
-                                            <RiComputerLine
-                                                className='education--icon'
-                                                size={screenSize.width > 575 ? 35 : 25} />
-                                            <PiComputerTowerBold
-                                                className='education--icon'
-                                                size={screenSize.width > 575 ? 35 : 25} />
-                                        </span>
-                                    </p>
-                                </Col>
-                                <Col>
-                                    <Link to={`https://www.csun.edu`} target='_blank'>
-                                        <Image src={CSUNLogo} className='school--logo' fluid />
-                                    </Link>
-                                </Col>
-                            </Col>
-                            <Col xs={12} sm={4} className='d-flex justify-content-end'>August 2022</Col>
-                        </Row>
-                        <Row className='mt-4'>
-                            <Col>
-                                <Col xs={12} sm={8}>
-                                    <p className='fw-bold'>Associate of Science in Web Development</p>
-                                </Col>
-                                <Col>
-                                    <Link to={`https://www.glendale.edu`} target='_blank'>
-                                        Glendale Community College
-                                    </Link>
-                                </Col>
-                            </Col>
-                            <Col xs={12} sm={4} className='d-flex justify-content-end'>August 2020</Col>
-                        </Row>
+                        <p>I have a Bachelore's degree
+                            <span className='degree--icon'>
+                                <PiCertificate
+                                    size={screenSize.width > 575 ? 35 : 25} />
+
+                            </span>
+                            in Computer Science
+                            <span className='education--icon'>
+                                <PiDesktop
+
+                                    size={screenSize.width > 575 ? 35 : 25} />
+                                <PiComputerTower
+                                    // className='education--icon'
+                                    size={screenSize.width > 575 ? 35 : 25} />
+                            </span>
+                            from the California State University - Northridge.</p>
                     </section>
                 </Container>
             </Container>
