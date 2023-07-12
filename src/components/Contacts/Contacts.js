5d115d8051bea08b9c70ec8c3da200d77323cd59
marditds <marditds@gmail.com>
2023-07-11 22:38:11 -0700

    contacts

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -28,15 +28,16 @@ export const Contacts = () => {
                     <section>
                         <h3>Contacts</h3>
                         <Row className='mt-2 '>
-                            <Col className='d-flex justify-content-center'>
+                            <Col xs={12} md={6} className='d-flex justify-content-center'>
                                 <Link className='d-grid cntcts--link' to={`mailto:${email}`}>
                                     <RxEnvelopeClosed className='mx-auto cntcts--icon email' size={widthSize.width > 575 ? 50 : 30} />
                                     marditds@gmail.com
                                 </Link>
                             </Col>
-                            <Col className='d-flex justify-content-center'>
+                            <Col xs={12} md={6} className='mt-4 mt-md-0 d-flex justify-content-center'>
                                 <Link
                                     to={'https://www.linkedin.com/in/mardit-dersoukiassian/'}
+                                    target='_blank'
                                     className='d-grid cntcts--link'>
                                     <SlSocialLinkedin className='mx-auto cntcts--icon linkedin' size={57} />
                                     /mardit-dersoukiassian
