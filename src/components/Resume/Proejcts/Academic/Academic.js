778225fa57685473450dee035ebb87a0e08cec60
marditds <marditds@gmail.com>
2023-07-12 16:38:06 -0700

    academic experience

--- src/components/Resume/Proejcts/Academic/Academic.js
+++ src/components/Resume/Proejcts/Academic/Academic.js
@@ -1,9 +1,40 @@
-import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom'
+import { Row, Col, Image } from 'react-bootstrap';
 import './Academic.css';
+import { academicData } from '../../../../data/academicData';
 
 export const Academic = () => {
+
+    const [acadData, setAcadData] = useState(academicData);
+
     return (
-        <div>Academic</div>
+        <Row>
+            {
+                acadData.map((item, index) => {
+                    return (
+                        <Row key={index}>
+                            <Col
+                                className={`${item.id}--col`}
+                            >
+                                <Link
+                                    to={item.url}
+                                    target='_blank'
+                                    className={`${item.id}--link`}
+                                >
+                                    <Image
+                                        src={item.avatar}
+                                        className={`${item.id}--logo`}
+                                        fluid
+                                    />
+                                    <p>{item.title}</p>
+                                </Link>
+                            </Col>
+                            <Col></Col>
+                        </Row>
+                    )
+                })
+            }
+        </Row>
     )
 }
--- src/data/academicData.js
+++ src/data/academicData.js
@@ -1,6 +1,10 @@
-export const logo = require('../assets', true);
+import { logos } from "./professionalData";
+
+export const logo = require.context('../assets', true);
+
 export const academicData = [{
     id: 'bitbeat',
     title: 'BitBeat Audio NFT Shop',
+    avatar: logos('./bitbeatLogo.png'),
     url: 'https://bitbeatshop.netlify.app'
 }]
