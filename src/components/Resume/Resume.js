3c6e4aaf1076eafad97b09ffb2ced6469bd008d1
marditds <marditds@gmail.com>
2023-07-26 22:38:13 -0700

    layout fix for mobile

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Resume.css';
+// import './Resume.css';
 import { Skills } from './Skills/Skills';
 import { Education } from './Education/Education';
 import { Projects } from './Proejcts/Projects';
